d2e509f1435852383e78208c29e1fb520a0cb61f
Maksim Putilov (Akvelon) <v-maputi@microsoft.com>
2016-07-17 00:38:33 +0300

    perf test

--- fsharp/ML/ML.Demo/Program.fs
+++ fsharp/ML/ML.Demo/Program.fs
@@ -6,10 +6,11 @@ open ML.Regressions.GLM
 open ML.Regressions.LinearRegression
 open ML.Regressions.BatchGradientDescent
 open MathNet.Numerics.LinearAlgebra
+open PerfUtil
 
 [<EntryPoint>]
 let main argv = 
-    let inputs, outputs = readCSV @"C:\dev\ml\machine-learning-ex1\ex1\ex1data2.txt" false [|0..1|] 2    
+    let inputs, outputs = readCSV @"..\..\..\..\..\machine-learning-ex1\ex1\ex1data2.txt" false [|0..1|] 2    
     let outputs = vector outputs
     let inputs, normPrms = norm  (matrix inputs)
     let model = {
@@ -22,7 +23,10 @@ let main argv =
         MinErrorThreshold = 0.
         Alpha = 0.01 
     }
-    let train = batchGradientDescent model prms inputs outputs
-    printfn "%A" train
+    let perf = Benchmark.Run (fun () ->
+        let train = batchGradientDescent model prms inputs outputs
+        printfn "result : %A" train
+    )    
+    printfn "perf : %A" perf
     System.Console.ReadLine() |> ignore
     0 // return an integer exit code
