f3f7763648a36fec16e7d84e5a85b528c84bf246
Maksim Putilov (Akvelon) <v-maputi@microsoft.com>
2016-07-27 23:31:49 +0300

    softmax

--- fsharp/ML/ML.Core/LinearAlgebra.fs
+++ fsharp/ML/ML.Core/LinearAlgebra.fs
@@ -8,7 +8,6 @@
     let spliceVector start count (vr: _ Vector) = 
         vr.SubVector(start, count)
 
-
     let zeros cnt = DenseVector.zero cnt
 
     let ones cnt = DenseVector.init cnt (fun _ -> 1.)
@@ -17,4 +16,20 @@
 
     let vecCons (el: _) (vec: _ Vector)  = Vector.insert 0 el vec
 
-    let appendOne (vec: _ Vector) = vecCons 1. vec 
+    let appendOne (vec: _ Vector) = vecCons 1. vec     
+
+    let repmatCol (cnt: int) (mx : _ Matrix) = 
+        Seq.init cnt (fun f -> mx.EnumerateColumns())
+        |> Seq.concat
+        |> Seq.toList 
+        |> List.map (fun v -> v.ToArray() |> List.ofArray)
+        |> matrix
+
+    let encodeOneHot (classesNum: int) (labels: float Vector) = 
+        List.init labels.Count (fun r -> 
+            List.init classesNum (fun c -> 
+                if labels.[r] = float c then 1. else 0.
+            )
+        )
+        |> matrix 
+        
---
+++ fsharp/ML/ML.Regressions/SoftmaxRegression.fs
@@ -0,0 +1,60 @@
+﻿module ML.Regressions.SoftmaxRegression
+
+open MathNet.Numerics.LinearAlgebra
+open ML.Core.Utils
+open ML.Core.LinearAlgebra
+
+open GLM
+
+let softmaxHyp (theta: float Matrix) (x: float Vector) = 
+    let sum = (theta * x).PointwiseExp() |> Vector.sum 
+    let exps = (theta .* x.ToColumnMatrix()).PointwiseExp()
+    (1. / sum) * exps
+
+let private softmax (theta: float Matrix) (x: float Matrix) =     
+    // n = number of features
+    // m = numbers of samples in x
+    // k = number of classes
+    // -------------------------
+    // theta : n * k
+    // x : n * m    
+    //---------------------------
+    // Returns : m * k
+    
+    // sum : m * 1
+    let sum = (x.Transpose() * theta).PointwiseExp().ColumnSums().ToRowMatrix()
+    // sumrep : m * k (all column values for each row are the same)
+    let sumrep = sum |> repmatCol theta.ColumnCount
+    // exps : m * k
+    let exps = (x.Transpose() * theta).PointwiseExp()
+    
+    sumrep ./ exps
+
+
+let softmaxCost (theta: float Matrix) (x : float Matrix) (y : float Vector) = 
+    // n = number of features
+    // m = numbers of samples in x
+    // k = number of classes
+    // -------------------------
+    // theta : n * k
+    // x : n * m    
+    //---------------------------
+    //Returns 1 * k
+
+    // p, lp : m * k
+    let logP = (softmax theta x).PointwiseLog()
+    
+    // mi : m * k
+    // one hot ?
+    // in each row only one column with index equal a class number is 1, allothers are zero
+    let oneHot = encodeOneHot theta.ColumnCount y 
+
+    // m * k : then sum all
+    oneHot .* logP |> Matrix.sumCols
+    
+
+let softmaxGradient (theta: float Matrix) (x: float Matrix) (y: float Vector) = 
+    let oneHot = encodeOneHot theta.ColumnCount y 
+    let p = softmax theta x
+    x * (oneHot - p) |> Matrix.sumCols |> (*) -1.
+
