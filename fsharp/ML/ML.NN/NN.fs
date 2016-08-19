1802720891bf1182b714cfed40487668554f49d7
Maksim Putilov (Akvelon) <v-maputi@microsoft.com>
2016-08-19 22:40:50 +0300

    backprop test

--- fsharp/ML/ML.NN/NN.fs
+++ fsharp/ML/ML.NN/NN.fs
@@ -68,21 +68,25 @@ let reshapeNN (shape: NNShape) (theta: FVector) : (FMatrix * ActivationFun) arra
     |> Stream.toArray
 
 type LayerForwardResult = {
+    //same as `W`
     Weights: FMatrix
+    //same as `z`
     Net : FVector
-    //Out is not required in hidden layers
+    //same as `a`
     Out : FVector
+    //Same as `f'`
     Activation : ActivationFun
 }
 
+//ForwardResult
 type LayerForward = 
-    | Input of FVector
+    | Input of FVector // inputs
     | Hidden of LayerForwardResult
 
 // returns a, z
 let calcLayerForward (theta: FMatrix) (activation: ActivationFun) (inputs: FVector) = 
-  let res1 = theta * (inputs |> appendOne) 
-  res1 |> activation.f, res1
+  let z = theta * (appendOne inputs) 
+  (activation.f z), z
   
 let forward2 (inputs: FVector) layers = 
     layers
@@ -102,16 +106,23 @@ let forward (inputs: FVector) (shape: NNShape) (theta: FVector) =
 
 type LayerBackwordResult = { Delta : FMatrix; Gradient : FMatrix }
 
+//BackpropResult
 type LayerBackword = 
     | LBNone
     | LBOutput of FMatrix * FVector // Weights of layer * Delta of output layer 
     | LBHidden of FMatrix * FVector * FMatrix // weights of layer * Delta * Gradient
     | LBInput of FMatrix // gradient
 
-let private caclGrads delta_l_plus_1 a_l = 
-    let outMx = [delta_l_plus_1] |> DenseMatrix.ofRowSeq |> appendOnes
-    let dOutMx = [a_l] |> DenseMatrix.ofColumnSeq
-    dOutMx * outMx
+let private caclGrads a_l delta_l_1 = 
+    let a_l_mx = [a_l] |> DenseMatrix.ofRowSeq |> appendOnes
+    let delta_l_1_mx = [delta_l_1] |> DenseMatrix.ofColumnSeq
+    delta_l_1_mx * a_l_mx
+
+let private caclHiddenDelta fwdLayer (weights_l_1: FMatrix) delta_l_1 = 
+    let delta_out = fwdLayer.Activation.f' fwdLayer.Net
+    let delta_l_1_mx = [delta_l_1] |> DenseMatrix.ofRowSeq
+    let delta_e = (delta_l_1_mx * weights_l_1.RemoveColumn(0)).Row(0)
+    delta_out .* delta_e
     
 let backprop (outputs: FVector) (inputs: FVector) (shape: NNShape) (theta: FVector) =
     
@@ -128,25 +139,25 @@ let backprop (outputs: FVector) (inputs: FVector) (shape: NNShape) (theta: FVect
             //ouput layer net
             let deltaOut = l.Activation.f' l.Net
             let deltaE = l.Out - outputs
-            let deltaRes = deltaOut .* deltaE
-            LBOutput(l.Weights, deltaRes)
-        | (Hidden l, LBOutput (weights, dOut)) ->
-            //last hidden layer
-            // gradient      
-            let grad = caclGrads l.Out dOut
-            // delta
-            let deltaOut = l.Activation.f' l.Net
-            let dOutMx = [dOut] |> DenseMatrix.ofRowSeq
-            let deltaE = (dOutMx * weights.RemoveColumn(0)).Row(0)
             let delta = deltaOut .* deltaE
+            LBOutput(l.Weights, delta)
+        | (Hidden l, LBOutput (weights_l_1, delta_l_1)) ->
+            //last hidden layer (n_l - 1)
+            let delta_l = caclHiddenDelta l weights_l_1 delta_l_1     
+            let grad_l = caclGrads l.Out delta_l_1
+            LBHidden(l.Weights, delta_l, grad_l)
+        | (Hidden l, LBHidden (weights_l_1, delta_l_1, _)) ->
+            //gradient for hidden layer (n_l - 2...)
+            let delta = caclHiddenDelta l weights_l_1 delta_l_1
+            let grad = caclGrads l.Out delta_l_1
             LBHidden(l.Weights, delta, grad)
-        | (Input inputs, LBHidden (weights, dOut, _)) ->
-            // gradient      
-            let grad = caclGrads inputs dOut
+        | (Input inputs, LBHidden (weights, delta_l_1, _)) ->
+            // calc gradient for first hidden layer (n_1)
+            let grad = caclGrads inputs delta_l_1
             LBInput(grad)
-        | (Input inputs, LBOutput (_, dOut)) ->
-            // one layer network case    
-            let grad = caclGrads inputs dOut
+        | (Input inputs, LBOutput (_, delta_l_1)) ->
+            // one layer network case (n_1)
+            let grad = caclGrads inputs delta_l_1
             LBInput(grad)
         | _ ->             
            failwith "not supported"
