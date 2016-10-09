b560b543d3d00f892dd68c5509d140c7dd5514fc
Maksim Putilov (Akvelon) <v-maputi@microsoft.com>
2016-10-09 16:53:13 +0300

    cores.stats

---
+++ fsharp/ML/ML.Core/Statistics.fs
@@ -0,0 +1,42 @@
+﻿module ML.Core.Statistics
+
+open System
+
+// Statistics monoids
+
+type Distr = { TotalSq : float;  Total : float; Count : float}
+
+let addDistr d1 d2 =
+    { TotalSq = d1.TotalSq + d2.TotalSq  ; Total = d1.Total + d2.Total ; Count = d1.Count + d2.Count }
+
+let (++) = addDistr
+
+let distr n = { TotalSq = n * n ; Total = n ; Count = 1. }
+
+let calcAvg d =
+    if d.Count = 0. then 0. else float d.Total / float d.Count
+
+//https://www.khanacademy.org/math/statistics-probability/displaying-describing-data/pop-variance-standard-deviation/v/statistics-alternate-variance-formulas
+
+let calcVarS d =
+    if d.Count <> 0. then 
+        let avg = calcAvg d
+        let cnt = d.Count - 1.
+        d.TotalSq / cnt - 2. * avg * d.Total / cnt + avg * avg * d.Count / cnt
+    else 
+        0.
+
+let calcVarP d =
+    if d.Count <> 0. then 
+        let avg = calcAvg d
+        d.TotalSq / d.Count - avg * avg
+    else 
+        0.
+
+let calcStdDevS = calcVarS >> Math.Sqrt
+
+let calcStdDevP = calcVarP >> Math.Sqrt
+
+let zero = { TotalSq = 0.;  Total = 0. ; Count = 0. }
+
+// Avg
---
+++ fsharp/ML/ML.Test/Core.Statistics.Tests.fs
@@ -0,0 +1,66 @@
+﻿module ML.Core.Statistics.Tests
+
+open Xunit
+open FsUnit
+open MathNet.Numerics.LinearAlgebra
+open NUnit.Framework.Constraints
+open ML.Core.Statistics
+open ML.Core.LinearAlgebra
+
+
+[<Fact>]
+let ``Calc avg 1. 3. should work``() =
+
+    let d = (distr 1.) ++ (distr 3.)
+
+    calcAvg d |> should equal 2.
+
+[<Fact>]
+let ``Calc avg 2. 5. should work``() =
+
+    let d = (distr 2.) ++ (distr 5.)
+
+    calcAvg d |> should equal 3.5
+
+[<Fact>]
+let ``Calc avg 0. 0. should work``() =
+
+    let d = (distr 0.) ++ (distr 0.)
+
+    calcAvg d |> should equal 0.
+
+[<Fact>]
+let ``Calc var p 1. 3. should work``() =
+
+    let d = (distr 1.) ++ (distr 3.)
+
+    let actual = calcVarP d 
+    
+    actual |> should equal 1.
+
+[<Fact>]
+let ``Calc var s 1. 3. should work``() =
+
+    let d = (distr 1.) ++ (distr 3.)
+
+    let actual = calcVarS d 
+    
+    actual |> should equal 2.
+
+[<Fact>]
+let ``Calc std dev p 1. 3. should work``() =
+
+    let d = (distr 1.) ++ (distr 3.)
+
+    let actual = calcStdDevP d 
+    
+    actual |> should equal 1.
+
+[<Fact>]
+let ``Calc std dev s 1. 3. should work``() =
+
+    let d = (distr 1.) ++ (distr 3.)
+
+    let actual = calcStdDevS d 
+    
+    actual |> should equal 1.4142135623730951
