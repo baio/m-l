436ddb4b5fdff3140e3ffe45bfc29a1046061fce
Maksim Putilov (Akvelon) <v-maputi@microsoft.com>
2016-10-22 13:52:47 +0300

    assets

--- fsharp/ML/ML.Demo/Program.fs
+++ fsharp/ML/ML.Demo/Program.fs
@@ -18,15 +18,15 @@ let main argv =
     MathNet.Numerics.Control.UseNativeMKL()        
 
     //logistic() |> ignore
-    softmax() |> ignore
+    //softmax() |> ignore
     //linear() |> ignore
     //DLinear() |> ignore
     //DLogistic() |> ignore
     //DSoftmax() |> ignore
-    //nn_xor()
+    nn_xor()
     //nn_mnist()
     //normalizeCsv "c:/dev/.data/mnist/mnist_train.csv" "c:/dev/.data/mnist/mnist_train_norm.csv" [0]
     //normalizeCsv "../../for_norm_test.csv" "../../for_norm_test_out.csv" [0]
 
-    System.Console.ReadLine() |> ignore
+    //System.Console.ReadLine() |> ignore
     0 // return an integer exit code
